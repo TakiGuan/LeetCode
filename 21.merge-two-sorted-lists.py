6da62ee2c29dccc2f366bc8a5d2e7b01fa35bdad
Taki Guan <taki.guan@gmail.com>
2021-01-04 22:19:47 +0800

    feat: :sparkles: add merge two sorted lists solution

---
+++ 21.merge-two-sorted-lists.py
@@ -0,0 +1,37 @@
+'''
+Description: Merge Two Sorted Lists
+Version: 1
+Author: Taki Guan
+Date: 2021-01-04 21:19:55
+LastEditors: Taki Guan
+LastEditTime: 2021-01-04 22:07:23
+'''
+#
+# @lc app=leetcode id=21 lang=python3
+#
+# [21] Merge Two Sorted Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # Recursive
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+# @lc code=end
